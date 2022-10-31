8a774203f8407f61330f53e30d8bb6fd2fffc2eb
ursanbia <ursan.bia@gmail.com>
2022-10-31 21:38:06 +0200

    inital commit

---
+++ src/test/java/ro/iteahome/travel/frontend/TravelFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.iteahome.travel.frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
